14b4fc28b907ceab12b4cd87fbab4298d415e144
Mehdi Azmani <mehdi@Mac.lan>
2025-03-17 02:10:08 +0100

    Préparation du déploiement

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../App.css';
-import Navbar from './components/utils/navbar';
-import HeroBanner from './components/Landing/Hero';
-import PartnerSection from './components/Landing/Sec-2';
-import PromoLivraison from './components/Landing/PromoLivraison';
-import ServicesSection from './components/Landing/Sec-4';
-import VivendiaComponent from './components/Landing/Sec-5';
-import ProductCatalogue from './components/Landing/Sec-6';
-import BestSellersSection from './components/Landing/Sec-7';
-import PassionEngagementSection from './components/Landing/Sec-8';
-import VivendiaBlogSection from './components/Landing/Sec-9';
-import ContactFormSection from './components/Landing/Sec-10';
-import Footer from './components/utils/Footer';
+import Navbar from '../components/utils/navbar';
+import HeroBanner from '../components/Landing/Hero';
+import PartnerSection from '../components/Landing/Sec-2';
+import PromoLivraison from '../components/Landing/PromoLivraison';
+import ServicesSection from '../components/Landing/Sec-4';
+import VivendiaComponent from '../components/Landing/Sec-5';
+import ProductCatalogue from '../components/Landing/Sec-6';
+import BestSellersSection from '../components/Landing/Sec-7';
+import PassionEngagementSection from '../components/Landing/Sec-8';
+import VivendiaBlogSection from '../components/Landing/Sec-9';
+import ContactFormSection from '../components/Landing/Sec-10';
+import Footer from '../components/utils/Footer';
 
 function Landing() {
   return (
